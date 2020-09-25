efb04d9eb6fc75655ee463a2afcf315bc60de493
Xiao-yang888 <714736690@qq.com>
2020-09-25 11:34:01 +0800

    qnmd

--- controllers/default.go
+++ controllers/default.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
+	"hellobeego/models"
+	"io/ioutil"
 )
 
 type MainController struct {
@@ -10,6 +13,9 @@ type MainController struct {
 }
 
 func (c *MainController) Get () {
+	//name := c.GetString("name")
+	//age,err := c.GetInt("age")
+
 	//获取Get类型请求的请求参数
 	name := c.Ctx.Input.Query("name")
 	age := c.Ctx.Input.Query("age")
@@ -23,31 +29,51 @@ func (c *MainController) Get () {
 	c.Ctx.ResponseWriter.Write([]byte("数据验证成功"))
 
 
-	c.Data["Website"] = "www.baidu.com"
-	c.Data["Email"] = "714736690@qq.com"
-	c.TplName = "index.tpl"
+	//c.Data["Website"] = "www.baidu.com"
+	//c.Data["Email"] = "714736690@qq.com"
+	//c.TplName = "index.tpl"
 }
 
 /**
 *该post方法是处理post类型的请求的时候要调用的方法
  */
-func (c *MainController) Post(){
-	fmt.Println("post类型的请求")
-	user := c.Ctx.Request.FormValue("user")
-	fmt.Println("用户名为：",user)
-	psd := c.Ctx.Request.FormValue("psd")
-	fmt.Println("密码是：",psd)
+//func (c *MainController) Post(){
+//	fmt.Println("post类型的请求")
+//	user := c.Ctx.Request.FormValue("user")
+//	fmt.Println("用户名为：",user)
+//	psd := c.Ctx.Request.FormValue("psd")
+//	fmt.Println("密码是：",psd)
+//
+//	//与固定值比较 用户名为：admin  密码为：123456
+//	if user != "admin" || psd != "123456"{
+//		//失败页面
+//		c.Ctx.ResponseWriter.Write([]byte("对不起，数据不正确"))
+//		return
+//	}
+//
+//	c.Ctx.ResponseWriter.Write([]byte("恭喜你，数据正确"))
+//	//request 请求  response 响应
+//	//c.Data["Website"] = "www.baidu.com"
+//	//c.Data["Email"] = "714736690@qq.com"
+//	//c.TplName = "index.tpl"
+//}
 
-	//与固定值比较 用户名为：admin  密码为：123456
-	if user != "admin" || psd != "123456"{
-		//失败页面
-		c.Ctx.ResponseWriter.Write([]byte("对不起，数据不正确"))
+func (c *MainController) Post(){
+	databyes, err := ioutil.ReadAll(c.Ctx.Request.Body)
+	if err != nil {
+		c.Ctx.WriteString("数据接收失败，请重试")
 		return
 	}
 
-	c.Ctx.ResponseWriter.Write([]byte("恭喜你，数据正确"))
-	//request 请求  response 响应
-	//c.Data["Website"] = "www.baidu.com"
-	//c.Data["Email"] = "714736690@qq.com"
-	//c.TplName = "index.tpl"
+	//json包解析
+	var person models.Person
+	err = json.Unmarshal(databyes,&person)
+	if err != nil{
+		c.Ctx.WriteString("数据解析失败，请重试")
+		return
+	}
+	fmt.Println("用户名",person.User,",年龄：",person.Age)
+	c.Ctx.WriteString("用户名是："+person.User)
+
 }
+
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	_ "hellobeego/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	//定义congig变量，接收并赋值为全局配置变量
+	config := beego.AppConfig
+	//获取配置选项
+	appname := config.String("appname")
+	fmt.Println("项目应用名称",appname)
+	port,err := config.Int("httpport")
+	if err != nil{
+		//配置信息解析错误
+		panic("项目配置信息解析错误，请查验后重试")
+	}
+	fmt.Println("应用监听窗口",port)
 	beego.Run()
 }
 
